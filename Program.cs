4a7b0fed970a3866866d75379e9d3daf00f33346
Guillermina <guillecarrascosuarez@gmail.com>
2024-05-21 01:38:40 -0300

    Ejercicio1

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a;
+int b;
+a=10;
+b=a;
+Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de b:"+b);
+
+//ejercicio1
+ Console.Write("Ingrese un número: ");
+        string input = Console.ReadLine();
+        
+
+        if (int.TryParse(input, out int number))
+        {
+            if (number > 0)
+            {
+
+                char[] arr = input.ToCharArray();
+                Array.Reverse(arr);
+                string reversedString = new string(arr);
+
+                int reversedNumber = int.Parse(reversedString);
+
+                Console.WriteLine("El número invertido es: " + reversedNumber);
+            }
+            else
+            {
+                Console.WriteLine("El número debe ser mayor a 0.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Por favor ingrese un número válido.");
+        }
+
+
